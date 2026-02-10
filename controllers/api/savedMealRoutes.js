e12cba411e4be7e5af90e2919c9a2056d8df75b7
tremckinley <tremckinley901@gmail.com>
2026-02-09 20:30:40 -0600

    basic landing page UI

--- controllers/api/savedMealRoutes.js
+++ controllers/api/savedMealRoutes.js
@@ -1,22 +1,4 @@
 const router = require("express").Router();
 //const { savedMeal } = require("../../models");
 
-// TODO: POST /api/savedMeal (logged in only)
-router.post("/", async (req, res) => {
-  try {
-    res.status(501).json({ message: "Not implemented yet" });
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
-
-// TODO: DELETE /api/savedMeal/:id (owner only)
-router.delete("/:id", async (req, res) => {
-  try {
-    res.status(501).json({ message: "Not implemented yet" });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
 module.exports = router;
