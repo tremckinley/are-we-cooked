664feb246101d0230500e59439f9ef3577ac0831
tremckinley <tremckinley901@gmail.com>
2026-02-13 06:20:02 -0600

    Configure env variables

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,26 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL, { dialect: "postgres", logging: false, dialectOptions: { ssl: { require: true, rejectUnauthorized: false } } })
-  : new Sequelize(
-      process.env.DB_NAME,
-      process.env.DB_USER,
-      process.env.DB_PASSWORD,
-      {
-        host: "localhost",
-        dialect: "postgres",
-        logging: false,
+const sequelize = process.env.DATABASE_URL || process.env.DB_URL
+  ? new Sequelize(process.env.DATABASE_URL || process.env.DB_URL, {
+    dialect: "postgres",
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
       },
-    );
+    },
+  })
+  : new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: "localhost",
+      dialect: "postgres",
+      logging: false,
+    },
+  );
 
 module.exports = sequelize;
